bde0713e983e3647ff54c84f06b5978537bf4781
avail <87807596+Abhayada@users.noreply.github.com>
2022-12-02 13:40:03 +0530

    Add files via upload

---
+++ adding.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,r;
+    cout<<"enter two digits:";
+    cin>>a;
+    cin>>b;
+
+    r=a+b;
+    cout<<"adding result:"<<r<<endl;
+
+    return 0;
+}
